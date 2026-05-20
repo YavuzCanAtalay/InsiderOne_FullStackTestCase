47aa0d20b7079969fe0f3374ff97a3063069ce8c
Yavuz Can Atalay <yavuzcanatalay@gmail.com>
2026-05-20 20:30:24 +0300

    simulator and services are implemented

--- InsiderOne_FullStackTestCase/internal/service/league_service.go
+++ InsiderOne_FullStackTestCase/internal/service/league_service.go
@@ -1,1 +1,105 @@
 package service
+
+import (
+	"github.com/YavuzCanAtalay/InsiderOne_FullStackTestCase/internal/domain"
+	"github.com/YavuzCanAtalay/InsiderOne_FullStackTestCase/internal/repository"
+	"github.com/YavuzCanAtalay/InsiderOne_FullStackTestCase/internal/simulator"
+)
+
+type LeagueService struct {
+	teamRepo  repository.TeamRepository
+	matchRepo repository.MatchRepository
+	simulator simulator.MatchSimulator
+}
+
+func NewLeagueService(
+	teamRepo repository.TeamRepository,
+	matchRepo repository.MatchRepository,
+	sim simulator.MatchSimulator,
+) *LeagueService {
+	return &LeagueService{
+		teamRepo:  teamRepo,
+		matchRepo: matchRepo,
+		simulator: sim,
+	}
+}
+
+// PlayNextWeek simulates the next unplayed week and returns results + updated standings
+func (s *LeagueService) PlayNextWeek() ([]domain.Match, []domain.Standing, error) {
+	unplayed, err := s.matchRepo.GetUnplayed()
+	if err != nil {
+		return nil, nil, err
+	}
+	if len(unplayed) == 0 {
+		return nil, nil, nil
+	}
+
+	nextWeek := unplayed[0].Week
+	var weekMatches []domain.Match
+	for _, m := range unplayed {
+		if m.Week == nextWeek {
+			weekMatches = append(weekMatches, m)
+		}
+	}
+
+	teams, err := s.teamRepo.GetAll()
+	if err != nil {
+		return nil, nil, err
+	}
+
+	teamMap := make(map[int]domain.Team)
+	for _, t := range teams {
+		teamMap[t.ID] = t
+	}
+
+	for _, m := range weekMatches {
+		result := s.simulator.Simulate(teamMap[m.HomeTeamID], teamMap[m.AwayTeamID])
+		if err := s.matchRepo.UpdateResult(m.ID, result); err != nil {
+			return nil, nil, err
+		}
+		hg, ag := result.HomeGoals, result.AwayGoals
+		m.HomeGoals = &hg
+		m.AwayGoals = &ag
+		m.IsPlayed = true
+	}
+
+	allMatches, err := s.matchRepo.GetAll()
+	if err != nil {
+		return nil, nil, err
+	}
+
+	standings := CalculateStandings(teams, allMatches)
+	return weekMatches, standings, nil
+}
+
+// PlayAll simulates all remaining weeks and returns results grouped by week
+func (s *LeagueService) PlayAll() (map[int][]domain.Match, []domain.Standing, error) {
+	results := make(map[int][]domain.Match)
+
+	for {
+		weekMatches, standings, err := s.PlayNextWeek()
+		if err != nil {
+			return nil, nil, err
+		}
+		if weekMatches == nil {
+			allMatches, _ := s.matchRepo.GetAll()
+			teams, _ := s.teamRepo.GetAll()
+			return results, CalculateStandings(teams, allMatches), nil
+		}
+		results[weekMatches[0].Week] = weekMatches
+		_ = standings
+	}
+}
+
+// GetStandings returns the current league table
+func (s *LeagueService) GetStandings() ([]domain.Standing, error) {
+	teams, err := s.teamRepo.GetAll()
+	if err != nil {
+		return nil, err
+	}
+	matches, err := s.matchRepo.GetAll()
+	if err != nil {
+		return nil, err
+	}
+	return CalculateStandings(teams, matches), nil
+}
--- InsiderOne_FullStackTestCase/internal/service/standings_service.go
+++ InsiderOne_FullStackTestCase/internal/service/standings_service.go
@@ -1,1 +1,72 @@
 package service
+
+import (
+	"sort"
+
+	"github.com/YavuzCanAtalay/InsiderOne_FullStackTestCase/internal/domain"
+)
+
+func CalculateStandings(teams []domain.Team, matches []domain.Match) []domain.Standing {
+	index := make(map[int]*domain.Standing)
+
+	for _, t := range teams {
+		t := t
+		index[t.ID] = &domain.Standing{
+			TeamID:   t.ID,
+			TeamName: t.Name,
+		}
+	}
+
+	for _, m := range matches {
+		if !m.IsPlayed || m.HomeGoals == nil || m.AwayGoals == nil {
+			continue
+		}
+
+		home := index[m.HomeTeamID]
+		away := index[m.AwayTeamID]
+		hg, ag := *m.HomeGoals, *m.AwayGoals
+
+		home.Played++
+		away.Played++
+		home.GoalsFor += hg
+		home.GoalsAgainst += ag
+		away.GoalsFor += ag
+		away.GoalsAgainst += hg
+		home.GoalDifference = home.GoalsFor - home.GoalsAgainst
+		away.GoalDifference = away.GoalsFor - away.GoalsAgainst
+
+		switch {
+		case hg > ag:
+			home.Won++
+			home.Points += 3
+			away.Lost++
+		case ag > hg:
+			away.Won++
+			away.Points += 3
+			home.Lost++
+		default:
+			home.Drawn++
+			away.Drawn++
+			home.Points++
+			away.Points++
+		}
+	}
+
+	standings := make([]domain.Standing, 0, len(teams))
+	for _, s := range index {
+		standings = append(standings, *s)
+	}
+
+	sort.Slice(standings, func(i, j int) bool {
+		a, b := standings[i], standings[j]
+		if a.Points != b.Points {
+			return a.Points > b.Points
+		}
+		if a.GoalDifference != b.GoalDifference {
+			return a.GoalDifference > b.GoalDifference
+		}
+		return a.GoalsFor > b.GoalsFor
+	})
+
+	return standings
+}
